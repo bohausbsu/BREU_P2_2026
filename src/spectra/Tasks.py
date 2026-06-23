3859e49da39977b13cf73dd777be637eef76f0c9
brad <bradleeborgholth@u.boisestate.edu>
2026-06-23 11:11:53 -0600

    added todos and docstrings to Task in Task.py

--- src/spectra/Tasks.py
+++ src/spectra/Tasks.py
@@ -5,13 +5,14 @@ from typing import Final
 
 from src.spectra.ProvenanceRecord import Entry
 
-# Represents a single task in a workflow.
-# Has unique taskID within the workflow,
-# a list of tasks that produce data inputs,
-# and a validity flag (not yet implemented)
-# A task should be uniquely identifiable through the combination
-# of its workflowID and taskID.
 class Task:
+    """
+    Represents a single task within a workflow. The task holds a list of workbase pointers to other tasks that serve as data inputs for this task. The task operates on the data inputs to produce 
+    its own data output, referenceable by latter tasks in a workbase. 
+    TODO:
+        - refactor to utilize locator style identification (wID, tID) only
+        - separate blueprint code from provenance code
+    """
 
     def __init__(self, workflowID, taskID, dataInput: list[Task] | Task | list[tuple[int, int]] | tuple[int, int] | None):
         
